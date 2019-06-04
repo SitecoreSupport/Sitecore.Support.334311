20499874977cec5a37eed42da8b6cce968c7e18a
iro <iro@sitecore.net>
2019-06-04 14:35:14 +0300

    Initial

---
+++ src/Sitecore.Support.334311/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.334311")]
+[assembly: AssemblyProduct("Sitecore.Support.334311")]
+[assembly: ComVisible(false)]
